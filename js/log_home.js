59c2f66a246485ae85dbf6564efe2026dec73f26
Emily de Souza Barbosa <sbemily21@gmail.com>
2023-06-16 21:51:38 -0300

    primeiro commit tela logado / home

---
+++ js/log_home.js
@@ -0,0 +1,14 @@
+/*let menu = document.getElementById("menu_links");
+let menu_icon = document.getElementById("menu_icon");
+
+function mostrarMenu (){
+
+    if (window.getComputedStyle(menu).display == "none") {
+        menu.style.display = "initial"
+        menu_icon.setAttribute("aria-label","fechar menu")
+    } else {
+        menu.style.display = "none"
+        menu_icon.setAttribute("aria-label","abrir menu")
+        menu_icon.setAttribute("aria-expanded", "false")
+    }
+}*/
